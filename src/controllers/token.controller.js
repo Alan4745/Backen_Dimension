efb13a8e6871d966dcc68270695cc7dc40d42c65
Eduardo-45 <alaneduardodelacruz407@gmail.com>
2025-02-08 23:19:55 -0600

    dejamos las probalidades normales

--- src/controllers/token.controller.js
+++ src/controllers/token.controller.js
@@ -630,18 +630,18 @@ async function redeemTicketPizzaCampero(req, res) {
     // Definir las categorías y sus probabilidades según el país
     const categoriesByCountry = {
       Guatemala: {
-        "PS5-G": 0,
-        "GIFTCARD-G-25": 0,
-        "GIFTCARD-G-50": 0,
-        "GIFTCARD-G-100": 0,
-        "PIZZA-G": 100,
+        "PS5-G": 0.00053,
+        "GIFTCARD-G-25": 0.00526,
+        "GIFTCARD-G-50": 0.00369,
+        "GIFTCARD-G-100": 0.00421,
+        "PIZZA-G": 0.03896,
       },
       "El Salvador": {
-        "PS5-S": 0,
-        "GIFTCARD-S-25": 0,
-        "GIFTCARD-S-50": 0,
-        "GIFTCARD-S-100": 0,
-        "PIZZA-S": 100,
+        "PS5-S": 0.0006988,
+        "GIFTCARD-S-25": 0.00699,
+        "GIFTCARD-S-50": 0.00559,
+        "GIFTCARD-S-100": 0.00489,
+        "PIZZA-S": 0.0517,
       },
     };
 
