f33692fec7c60062ceb5b3b68151f3dd9bfba4d2
Eduardo-45 <alaneduardodelacruz407@gmail.com>
2025-03-03 00:57:33 -0600

    quitamos los intentos

--- src/controllers/token.controller.js
+++ src/controllers/token.controller.js
@@ -790,12 +790,12 @@ async function redeemTicketChokis(req, res) {
       });
     }
 
-    if (chokisData.ticketsCollected.length >= 3) {
-      console.log("El comprador ya ha alcanzado el límite de intentos.");
-      return res.status(400).json({
-        message: "Ya se te acabaron los intentos.",
-      });
-    }
+    // if (chokisData.ticketsCollected.length >= 3) {
+    //   console.log("El comprador ya ha alcanzado el límite de intentos.");
+    //   return res.status(400).json({
+    //     message: "Ya se te acabaron los intentos.",
+    //   });
+    // }
 
     // Definir las categorías y sus probabilidades
     const categories = {
